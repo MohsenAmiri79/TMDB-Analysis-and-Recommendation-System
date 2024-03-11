f36453e4cbae579783f3b7c7081a41704c312003
MohsenAmiri79 <mohsenamiri79@yahoo.com>
2024-03-11 23:05:19 +0330

    Cleaned genres.

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,4 +1,4 @@
-def print_as_table(lst, width):
+def multiline_print(lst, width=5):
     for i in range(0, len(lst), width):
         row = lst[i:i+width]
         formatted_row = ' | '.join(map(str, row))
