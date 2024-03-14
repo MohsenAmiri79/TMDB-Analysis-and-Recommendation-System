8d84bdd1ce6e534cacae6d2ff8b5deae03715367
MohsenAmiri79 <mohsenamiri79@yahoo.com>
2024-03-14 17:17:57 +0330

    Completed EDA.

--- utilities/utils.py
+++ utilities/utils.py
@@ -4,3 +4,9 @@ def multiline_print(lst, width=5):
         formatted_row = ' | '.join(map(str, row))
         print(f'| {formatted_row} |')
     print("")
+
+
+def move_to_front(lst, element):
+    if element in lst:
+        lst.insert(0, lst.pop(lst.index(element)))
+    return lst
