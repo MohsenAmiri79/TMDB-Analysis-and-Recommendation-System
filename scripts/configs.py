eb2142cceacbe9aa2c5c584339593935a8302be8
MohsenAmiri79 <mohsenamiri79@yahoo.com>
2024-03-15 23:33:32 +0330

    finalized project.

---
+++ scripts/configs.py
@@ -0,0 +1,46 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.colors as mcolors
+
+
+# Colormaps
+def DIS_CMAP(bins=30, r=False):
+    if r:
+        return list(reversed([CON_CMAP(i) for i in np.linspace(0, 1, bins)]))
+    return [CON_CMAP(i) for i in np.linspace(0, 1, bins)]
+
+
+CON_CMAP = sns.cubehelix_palette(start=-1.5, rot=.5, as_cmap=True)
+
+DIV_CMAP = mcolors.LinearSegmentedColormap.from_list(
+    "mirrored_cmap",
+    colors=np.concatenate((CON_CMAP(np.linspace(0, 1, 128)),
+                           CON_CMAP(np.linspace(1, 0, 128)))))
+
+
+# Column Lists
+NON_STR_COLS = [
+    'budget', 'popularity', 'runtime', 'vote_average',
+    'vote_count', 'Action', 'Adventure', 'Animation', 'Comedy',
+    'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Foreign',
+    'History', 'Horror', 'Music', 'Mystery', 'Romance',
+    'Science Fiction', 'TV Movie', 'Thriller', 'War', 'Western',
+    'language_code', 'status_code', 'year', 'month', 'day',
+    'actor_1_code', 'actor_2_code', 'actor_3_code', 'actor_4_code',
+    'actor_5_code', 'production_company_code',
+]
+CREW_COLS = [
+    'actor_1_code', 'actor_2_code', 'actor_3_code',
+    'actor_4_code', 'actor_5_code', 'production_company_code',
+]
+GENRE_COLS = [
+    'Action', 'Adventure', 'Animation', 'Comedy', 'Crime',
+    'Documentary', 'Drama', 'Family', 'Fantasy', 'Foreign',
+    'History', 'Horror', 'Music', 'Mystery', 'Romance',
+    'Science Fiction', 'TV Movie', 'Thriller', 'War', 'Western'
+]
+NON_GENRECREW_COLS = list(set(NON_STR_COLS) - set(GENRE_COLS) - set(CREW_COLS))
+NON_GENRE_COLS = list(set(NON_STR_COLS) - set(GENRE_COLS))
+NUMERICAL_COLS = list(set(NON_GENRECREW_COLS)
+                      - set(['language_code', 'status_code']))
+LABEL_ENC_COLS = CREW_COLS + ['language_code', 'status_code']
---
+++ scripts/inference.py
@@ -0,0 +1,69 @@
+import pandas as pd
+
+from joblib import load
+from os.path import join as joinpath
+
+# Constants
+from scripts.configs import NON_GENRE_COLS, GENRE_COLS
+from scripts.configs import NUMERICAL_COLS, LABEL_ENC_COLS
+
+
+MODEL_PATH = "./models/"
+
+
+class ModelInference:
+    def __init__(self, model_path=MODEL_PATH):
+        self.models = {
+            'lr': load(joinpath(model_path,
+                                'logistic_regression_model.joblib')),
+            'dt': load(joinpath(model_path,
+                                'decision_tree_model.joblib')),
+            'rf': load(joinpath(model_path,
+                                'random_forest_model.joblib')),
+            'xgb': load(joinpath(model_path,
+                                 'xgboost_model.joblib')),
+        }
+
+        self.preps = {
+            'genrePCA': load(joinpath(model_path,
+                                      'genre_pca.joblib')),
+            'label_scale': load(joinpath(model_path,
+                                         'label_scaler.joblib')),
+            'numeric_norm': load(joinpath(model_path,
+                                          'numerical_scaler.joblib')),
+            'feature_selector': load(joinpath(model_path,
+                                              'feature_selector.joblib')),
+        }
+
+    def clean_sample(self, sample):
+        numeric_cols = ['revenue'] + NUMERICAL_COLS
+        sample[numeric_cols] = \
+            self.preps['numeric_norm'].transform(sample[numeric_cols])
+
+        sample[LABEL_ENC_COLS] = \
+            self.preps['label_scale'].transform(sample[LABEL_ENC_COLS])
+
+        principal_components = \
+            self.preps['genrePCA'].transform(sample[GENRE_COLS])
+        principalDf = pd.DataFrame(data=principal_components,
+                                   columns=[f'genrePC{i+1}' for i in range(5)])
+
+        x = sample[NON_GENRE_COLS]
+        X_new = self.preps['feature_selector'].transform(x)
+        cols = self.preps['feature_selector'].get_support(indices=True)
+        selected_features_df = x.iloc[:, cols]
+
+        return pd.concat([selected_features_df, principalDf], axis=1)
+
+    def predict(self, sample, model_name):
+        model = self.models[model_name]
+
+        sample = self.clean_sample(sample).values
+
+        if len(sample.shape) == 1:
+            sample = sample.reshape(1, -1)
+
+        prediction = model.predict(sample)
+        print('eeeee')
+        print(model.classes_)
+        return prediction
